e2ebb0f83473b48a78680c69d4b992162ba1de15
Jaagup Kippar <jaagup@tlu.ee>
2025-04-02 06:03:30 +0000

    esimene takisti

---
+++ ryhm1/resistorrun1.js
@@ -0,0 +1,12 @@
+var Resistor = /** @class */ (function () {
+    function Resistor(r) {
+        this.r = 0;
+        this.r = r;
+    }
+    Resistor.prototype.getCurrent = function (u) {
+        return u / this.r;
+    };
+    return Resistor;
+}());
+var r1 = new Resistor(220);
+console.log(r1.getCurrent(5));
